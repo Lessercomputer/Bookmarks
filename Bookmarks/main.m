062904f6e8269b2eec9e6cfd2161b3b81e0c17da
akifumi <akifumi@mac.com>
2014-05-04 07:45:23 +0900

    Initial Commit

---
+++ Bookmarks/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Bookmarks
+//
+//  Created by 高田 明史 on 2014/05/04.
+//  Copyright (c) 2014年 Pedophilia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
